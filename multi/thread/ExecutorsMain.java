3120df9c21fbb7386af80eba44d258527240aae9
JumpingNotes <lewislichengyong@gmail.com>
2017-07-11 17:12:15 +0800

    Update ExecutorsMain.java

--- multi/thread/ExecutorsMain.java
+++ multi/thread/ExecutorsMain.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 线程池的简单使用A
+ * 线程池的简单使用
  * @author 鱼唇的人类
  * @date 2017/5/20
  * @time 22:28
@@ -16,7 +16,7 @@ public class ExecutorsMain {
     public static void main(String[] args) {
         ExecutorService threadPool = Executors.newCachedThreadPool();
         threadPool= Executors.newFixedThreadPool(3);
-        int []timeSpans={1,2,4,7,9,3,6,8,12,5,23,15};
+        int []timeSpans={1,2,4,7,9,3,6,8,12,5,23,15,17};
         System.out.println("理论使用时间："+Arrays.stream(timeSpans).sum()+"ms");
         for (int i = 0; i < timeSpans.length; i++) {
             final int timeSpan= timeSpans[i];
